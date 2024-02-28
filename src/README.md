08de83934692a2f1adf85c8143daaf3dcd756e51
JongKook <whdrnrdl789@naver.com>
2024-02-29 00:53:31 +0900

    Simple Steam Api

---
+++ src/README.md
@@ -0,0 +1,13 @@
+# STEAM API 사용법
+
+### 공식문서 
+[API 문서](https://partner.steamgames.com/doc/webapi)
+
+### 예시
+POST https://partner.steam-api.com/IBroadcastService/PostGameDataFrame/v1/
+<br>
+partner.steam-api.com -> api.steampowered.com 로 수정하면 403 에러가 뜨지 않음
+<br>
+``` curl
+api.steampowered.com/
+```
--- src/main/java/com/example/steam/SteamApiService.java
+++ src/main/java/com/example/steam/SteamApiService.java
@@ -1,2 +1,31 @@
-package com.example.steam;public class SteamApiService {
+package com.example.steam;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class SteamApiService {
+    public void getSummary(String apiKey){
+        WebClient webClient = WebClient.builder().build();
+        String url ="https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" +apiKey +"&steamids=76561197960435530";
+        String response = webClient.get()
+                .uri(url)
+                .header("x-test", "header")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        System.out.println(response);
+    }
+
+    public void getRecentlyPlayedGames(String apiKey, String steamId, int count){
+        WebClient webClient = WebClient.builder().build();
+        String url ="https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v1/?key=" + apiKey + "&steamid=" + steamId + "&count=" + count;
+        String response = webClient.get()
+                .uri(url)
+                .header("x-test", "header")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        System.out.println(response);
+    }
 }
